33325114c7156bb6b651b5ce539419074bc74b29
Anondor <islamazizul0000@gmail.com>
2023-03-08 20:58:08 +0600

    second commit (End of section 3)

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,38 @@
+
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { Container } from "@mui/system";
+import { useState } from "react";
+import Catalog from "../../features/catalog/Catalog";
+import Header from "./Header";
+
+function App() {
+  const[darkMode,setDarkMode]=useState(false);
+  const palatteType=darkMode?'dark':'light';
+  const theme=createTheme({
+    palette:{
+      mode: palatteType,
+      background:{
+        default: palatteType==='light'?'#eaeaea':'#121212'
+      }
+    }
+
+  })
+
+  function handleThemeChange(){
+    setDarkMode(!darkMode);
+  }
+  
+  return (
+    <ThemeProvider theme={theme}>
+    <CssBaseline/>
+    <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
+    <Container>
+       <Catalog /> 
+    </Container>
+
+    </ThemeProvider>
+  );
+}
+
+export default App;
+  
